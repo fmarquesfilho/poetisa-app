f14b098939da64ce74316dcec6a51b6eed3daf6c
Fernando Figueira <690686+fmarquesfilho@users.noreply.github.com>
2024-11-24 16:03:41 -0300

    add step sequencer frontend code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,11 @@
 <script lang="ts">
   import { invoke } from "@tauri-apps/api/core";
 
-  let name = $state("");
-  let greetMsg = $state("");
-
-  async function greet(event: Event) {
-    event.preventDefault();
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    greetMsg = await invoke("greet", { name });
-  }
+  let bpm = $state(100);
+  let beat = $state(0);
+  const beatsPerMeasure = 4;
+  const numberOfMeasures = 4;
+  let isPlaying = $state(false);
 
   let message = $state('');
   let result = $state('');
@@ -22,108 +19,169 @@
       message = `Error: ${error}`;
     }
   }
+
+  const filenames = ["clap.ogg", "hihat.ogg", "kick.ogg", "snare.ogg"];
+
+  let instruments = [
+    Array.from({ length: beatsPerMeasure*numberOfMeasures }, (_, i) => ({ note: filenames[3], active: false })),
+    Array.from({ length: beatsPerMeasure*numberOfMeasures }, (_, i) => ({ note: filenames[2], active: false })),
+    Array.from({ length: beatsPerMeasure*numberOfMeasures }, (_, i) => ({ note: filenames[1], active: false })),
+    Array.from({ length: beatsPerMeasure*numberOfMeasures }, (_, i) => ({ note: filenames[0], active: false })),
+  ];
+
+  const handleNoteClick = (instIndex: number, noteIndex: number) => {
+    instruments[instIndex][noteIndex].active = !instruments[instIndex][noteIndex].active;
+  };
+
+  const handlePlayClick = () => {
+    isPlaying = true;
+  };
+
+  const handleStopClick = () => {
+    isPlaying = false;
+  };
 </script>
 
+<style>
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+      Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+
+    font-size: 16px;
+    line-height: 24px;
+    font-weight: 400;
+    background-color: #36485a;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+
+  .sequencer__wrapper {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    gap: 8px;
+    background-color: #98c9fa;
+    border-radius: 7px;
+    padding: 4px;
+  }
+
+  @media (min-width: 768px) and (max-width: 1023px) {
+    .sequencer__wrapper {
+      grid-template-columns: repeat(2, 1fr);
+    }
+  }
+
+  @media (max-width: 767px) {
+    .sequencer__wrapper {
+      grid-template-columns: 1fr;
+    }
+  }
+
+  .sequencer__block {
+    display: block;
+    margin: 5px;
+  }
+
+  .sequencer__measure {
+    display: flex;
+    gap: 5px;
+    border-color: #555;
+  }
+
+  .sequencer__beat {
+    display: block;
+  }
+
+  .note {
+    background: #ccc;
+    width: 70px;
+    height: 50px;
+    border: 1px solid #ccc;
+    border-radius: 7px;
+    margin-top: 7px;
+  }
+
+  .note.active {
+    background: #600889;
+    border: 1px solid #600889;
+    color: #fff;
+  }
+
+  .first-beat-of-the-bar {
+    background: #434445;
+    border: 1px solid #98c9fa;
+  }
+
+  .bpm-controls {
+    margin-bottom: 20px;
+    text-align: center;
+  }
+
+  .bpm-controls label {
+    color: #fff;
+  }
+
+  .beat-indicator {
+    width: 10px;
+    height: 10px;
+    border-radius: 50%;
+    background: #555;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #fff;
+    font-size: 1.5rem;
+    margin: 0 auto;
+  }
+
+  .live {
+    background: #05f18f;
+  }
+
+</style>
+
 <main class="container">
-  <h1>Step Sequencer</h1>
+
+  <div class="bpm-controls">
+    <label for="bpm">{bpm} BPM</label>
+    <input type="range" id="bpm" min="50" max="240" bind:value={bpm} />
+    {#if isPlaying}
+      <button onclick={handleStopClick}>Stop</button>
+    {:else}
+      <button onclick={handlePlayClick}>Play</button>
+    {/if}
+  </div>
+
+  <div class="sequencer">
+    <div class="sequencer sequencer__wrapper">
+      {#each Array(numberOfMeasures) as _, measureIndex}
+      <div class="sequencer sequencer__block">
+        {#each Array(instruments.length) as _, instrumentIndex}
+        <div class="sequencer sequencer__measure">
+            {#each Array(beatsPerMeasure) as _, beatIndex}
+            <div class="sequencer sequencer__beat">
+              {#if instrumentIndex === 0}
+                <div class="beat-indicator {(measureIndex*beatsPerMeasure)+beatIndex === beat ? 'live' : ''}"></div>
+              {/if}
+              <!-- svelte-ignore a11y_consider_explicit_label -->
+              <button 
+                onclick={() => handleNoteClick(instrumentIndex, (measureIndex*beatsPerMeasure)+beatIndex)}
+                class="note {instruments[instrumentIndex][(measureIndex*beatsPerMeasure)+beatIndex].active ? 'active' : ''} {beatIndex % 4 === 0 ? 'first-beat-of-the-bar' : ''}">
+              </button>
+            </div>
+            {/each}
+        </div>
+        {/each}
+      </div>
+      {/each}
+    </div>
+  </div>
+
   <form class="row" onsubmit={play_audio}>
     <button type="submit">Play Sound</button>
     <p>{message}</p>
     <p>{result}</p>
   </form>
-
-  <form class="row" onsubmit={greet}>
-    <input id="greet-input" placeholder="Enter a name..." bind:value={name} />
-    <button type="submit">Greet</button>
-  </form>
-  <p>{greetMsg}</p>
 </main>
 
-<style>
-
-:root {
-  font-family: Inter, Avenir, Helvetica, Arial, sans-serif;
-  font-size: 16px;
-  line-height: 24px;
-  font-weight: 400;
-
-  color: #0f0f0f;
-  background-color: #f6f6f6;
-
-  font-synthesis: none;
-  text-rendering: optimizeLegibility;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-text-size-adjust: 100%;
-}
-
-.container {
-  margin: 0;
-  padding-top: 10vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  text-align: center;
-}
-
-.row {
-  display: flex;
-  justify-content: center;
-}
-
-h1 {
-  text-align: center;
-}
-
-input,
-button {
-  border-radius: 8px;
-  border: 1px solid transparent;
-  padding: 0.6em 1.2em;
-  font-size: 1em;
-  font-weight: 500;
-  font-family: inherit;
-  color: #0f0f0f;
-  background-color: #ffffff;
-  transition: border-color 0.25s;
-  box-shadow: 0 2px 2px rgba(0, 0, 0, 0.2);
-}
-
-button {
-  cursor: pointer;
-}
-
-button:hover {
-  border-color: #396cd8;
-}
-button:active {
-  border-color: #396cd8;
-  background-color: #e8e8e8;
-}
-
-input,
-button {
-  outline: none;
-}
-
-#greet-input {
-  margin-right: 5px;
-}
-
-@media (prefers-color-scheme: dark) {
-  :root {
-    color: #f6f6f6;
-    background-color: #2f2f2f;
-  }
-
-  input,
-  button {
-    color: #ffffff;
-    background-color: #0f0f0f98;
-  }
-  button:active {
-    background-color: #0f0f0f69;
-  }
-}
-
-</style>
