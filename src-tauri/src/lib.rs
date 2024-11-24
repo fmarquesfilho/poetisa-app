d8ed7c4cb07c92e49ec329d167c7b6a946366c13
Fernando Figueira <690686+fmarquesfilho@users.noreply.github.com>
2024-11-24 09:43:28 -0300

    implement test code with kira

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,15 @@
+
+mod sequencer;
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+#[tauri::command]
+fn play_audio() -> Result<(), String> {
+    sequencer::play_audio().map_err(|e| e.to_string())
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
@@ -9,6 +17,7 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![play_audio])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
---
+++ src-tauri/src/sequencer.rs
@@ -0,0 +1,26 @@
+use kira::{
+	manager::{
+		AudioManager, AudioManagerSettings,
+		backend::DefaultBackend,
+	},
+	sound::static_sound::StaticSoundData,
+	clock::ClockSpeed,
+};
+
+pub fn play_audio() -> Result<(), Box<dyn std::error::Error>> {
+    const TEMPO: f64 = 120.0;
+
+    let mut manager = AudioManager::<DefaultBackend>::new(AudioManagerSettings::default())?;
+    let mut clock = manager.add_clock(ClockSpeed::TicksPerMinute(TEMPO))?;
+    let sound_data_1 = StaticSoundData::from_file("src/audio/hihat.ogg")?
+        .start_time(clock.time() + 2);
+    manager.play(sound_data_1)?;
+    let sound_data_2 = StaticSoundData::from_file("src/audio/snare.ogg")?
+        .start_time(clock.time() + 4);
+    manager.play(sound_data_2)?;
+    clock.start();
+
+    std::thread::sleep(std::time::Duration::from_millis(1000000000));
+
+    Ok(())
+}
